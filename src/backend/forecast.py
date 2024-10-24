6def3322bb48083ecbc5849d54725ac3f894d2ff
Nikita <savvin.nikit@yandex.ru>
2024-10-24 20:07:27 +0300

    remove path

--- src/backend/forecast.py
+++ src/backend/forecast.py
@@ -254,10 +254,10 @@ def forecast(
     df_real_predict['minute'] = df_real_predict['minute'].fillna(method='ffill')
     df_real_predict['second'] = df_real_predict['second'].fillna(method='ffill')
 
-
-    df_evaluetion.to_csv('/Users/nikitasavvin/Desktop/Учеба/tool_backend/experiments/df_evaluetion.csv')
-    df_true_all_col.to_csv('/Users/nikitasavvin/Desktop/Учеба/tool_backend/experiments/df_true_all_col.csv')
-    df_real_predict.to_csv('/Users/nikitasavvin/Desktop/Учеба/tool_backend/experiments/df_real_predict.csv')
+    #
+    # df_evaluetion.to_csv('/Users/nikitasavvin/Desktop/Учеба/tool_backend/experiments/df_evaluetion.csv')
+    # df_true_all_col.to_csv('/Users/nikitasavvin/Desktop/Учеба/tool_backend/experiments/df_true_all_col.csv')
+    # df_real_predict.to_csv('/Users/nikitasavvin/Desktop/Учеба/tool_backend/experiments/df_real_predict.csv')
 
     response_code, response_massage = 200, 'The training was successful'
     return df_evaluetion, df_true_all_col, loss_list, df_real_predict, response_code, response_massage
