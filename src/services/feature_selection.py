3922a266b1b754d8d638931082ebda7041a7f1ac
anastasiashakhov-a <anastasia.shakhova1@gmail.com>
2025-07-01 00:05:46 +0300

    feat(config): add YAML file for feature selection

--- src/services/feature_selection.py
+++ src/services/feature_selection.py
@@ -1,13 +1,30 @@
 #src/services/feature_selection.py
 import pandas as pd
 import numpy as np
+import yaml
+
 from tqdm import tqdm
 from typing import List, Dict
+from pathlib import Path
 from src.normalization.time2vec import Time2Vec
 from src.processing.data_processing import calculate_time_interval
 from src.models.xgboost_model import forecast_XGBoost_sistem
 from src.utils.metrics import calculate_metrics
 
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent  # Корень проекта
+CONFIG_DIR = PROJECT_ROOT / "src" / "config"
+
+def load_possible_cols():
+    config_path = CONFIG_DIR / "possible_cols.yaml"
+    try:
+        with open(config_path, 'r') as file:
+            config = yaml.safe_load(file)
+        return config.get('all_possible_cols', [])
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Config file not found at {config_path}")
+    except yaml.YAMLError as e:
+        raise ValueError(f"Error parsing YAML file: {e}")
+    
 def col_selection_xgboots(
         df_init: pd.DataFrame,
         time_column: str,
@@ -23,12 +40,14 @@ def col_selection_xgboots(
     :param lag: Количество временных шагов для лаговых признаков.
     :return: Словарь с выбранными признаками и значением MAPE.
     """
-    # Сохранение оригинального формата временной колонки
+    all_possible_cols = load_possible_cols()  # Используем функцию load_possible_cols
+
+    # Остальной код остается неизменным
     original_column = df_init[time_column].copy()
     df_init[time_column] = pd.to_datetime(df_init[time_column], errors='coerce')
     df_init = df_init.sort_values(by=time_column).reset_index(drop=True)
     df_init[time_column] = original_column[df_init.index]
-    
+
     # Определение размера тестовой выборки
     last_value = df_init[col_target].iloc[0]
     optimal_evaluation_points = 300
@@ -38,32 +57,22 @@ def col_selection_xgboots(
     df = df_init[:-optimal_evaluation_points]
     df_empty = df_evaluation.copy()
     df_empty[col_target] = None
-    
+
     # Объединение данных для нормализации
     df_all_data = pd.concat([df, df_empty], ignore_index=True).sort_values(by=time_column).reset_index(drop=True)
     last_known_index = len(df_all_data) - optimal_evaluation_points
-    
+
     # Нормализация данных
     t2v = Time2Vec(col_time=time_column, col_target=col_target)
     df_all_data_norm, min_val, max_val = t2v.vectorization(df_all_data)
     df_all_data_norm = df_all_data_norm.sort_values(by=time_column).reset_index(drop=True)
-    
-    # Список всех возможных признаков
-    all_possible_cols = [
-        "year", "month", "day", "week", "day_of_week", "hour", "minute", "second",
-        "hour_sin", "hour_cos", "day_of_week_sin", "day_of_week_cos",
-        "week_sin", "week_cos", "month_sin", "month_cos",
-        "part_of_day", "is_night", "is_weekend", "day_of_year",
-        "is_working_hours", "season", "season_sin", "season_cos",
-        "quarter", "quarter_sin", "quarter_cos", "moon_phase",
-    ]
-    
+
     # Поиск лучших признаков
     col_for_train = []
     best_mape = float('inf')
     df_evaluation[time_column] = pd.to_datetime(df_evaluation[time_column], errors='coerce')
     df_evaluation = df_evaluation.sort_values(by=time_column).reset_index(drop=True)
-    
+
     for col in tqdm(all_possible_cols):
         current_cols = col_for_train + [col]
         df_true_all, df_pred_vector = forecast_XGBoost_sistem(
@@ -79,16 +88,16 @@ def col_selection_xgboots(
         df_real_predict[col_target] = df_real_predict[col_target].astype('float64')
         df_real_predict.at[df_real_predict.index[-1], col_target] = last_value
         df_real_predict[time_column] = df_evaluation[time_column]
-        
+
         y_true = df_evaluation[col_target].reset_index(drop=True)
         y_pred = df_real_predict[col_target].reset_index(drop=True)
         _, _, _, mape, _ = calculate_metrics(y_true=y_true, y_pred=y_pred)
-        
+
         print(f"CURRENT MAPE = {mape} | BEST MAPE = {best_mape}")
         if mape < best_mape:
             best_mape = mape
             col_for_train.append(col)
-    
+
     return {"col_for_train": col_for_train, "best_mape": best_mape}
 
 def lag_selection_xgboots(
