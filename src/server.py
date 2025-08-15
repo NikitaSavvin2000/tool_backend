cdbb6266dee033bf1836a7b15ba3ca8f849f8882
Nikita <savvin.nikit@yandex.ru>
2025-08-15 11:20:06 +0300

    fast save

--- src/server.py
+++ src/server.py
@@ -15,8 +15,11 @@ from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from src.core.configuration.config import settings
 from src.core.logger import logger
 from src.routers.api_router import api_router
+from src.core.token import token_validator
+
 from src.routers.forecast_router import router as forecast_router
 
+API_PREFIX = "/tool_backend"
 
 # Загрузка переменных окружения
 load_dotenv()
@@ -33,48 +36,14 @@ logger.info(f"[WORKERS] Count workers = {workers}")
 # Инициализация системы безопасности
 security = HTTPBearer()
 
-# Проверка и загрузка токенов
-try:
-    tokens_link = os.getenv("TOKEN_LIST")
-    if not tokens_link:
-        raise ValueError("Environment variable TOKEN_LIST is not set or empty.")
-
-    logger.info(f"Loading tokens from: {tokens_link}")
-    with open(tokens_link, 'r', encoding='utf-8') as file:
-        tokens_data = yaml.safe_load(file)
-
-    if not tokens_data or "tokens" not in tokens_data:
-        raise ValueError("Tokens file is empty or does not contain the 'tokens' key.")
-
-    VALID_TOKENS = [
-        token["token"] for token in tokens_data["tokens"]
-        if token.get("source") == "tool_backend"
-    ]
-
-    if not VALID_TOKENS:
-        raise ValueError("No valid tokens found for 'tool_backend' source.")
-    
-    logger.info("Tokens loaded successfully.")
-except Exception as e:
-    logger.error(f"Failed to load tokens: {e}")
-    sys.exit(1)
-
-def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
-    token = credentials.credentials
-    if token not in VALID_TOKENS:
-        logger.warning(f"Unauthorized access attempt with token: {token[:5]}...")
-        raise HTTPException(
-            status_code=401,
-            detail="Unauthorized.",
-        )
-    logger.info(f"Token verified successfully: {token[:5]}...")
-    return token
 
 # Создание FastAPI приложения
 docs_url = "/docs"
 app = FastAPI(
     docs_url=docs_url,
-    dependencies=[Depends(verify_token)],
+    openapi_url="/openapi.json",
+    root_path=API_PREFIX,
+    dependencies=[Depends(token_validator)] if settings.VERIFY_TOKEN else []
 )
 
 @app.exception_handler(RequestValidationError)
@@ -112,15 +81,14 @@ def read_root():
 
 if __name__ == "__main__":
     try:
-        port = 7070
-        logger.info(f"Starting server on http://0.0.0.0:{port}{docs_url}")
+        logger.info(f"Starting server on http://{settings.HOST}:{settings.PORT}")
+        print(f'🚀 Документация http://0.0.0.0:{settings.PORT}{API_PREFIX}/docs')
         uvicorn.run(
-            "server:app",
-            host="0.0.0.0",
-            port=port,
+            "src.server:app",
+            host=settings.HOST,
+            port=settings.PORT,
             workers=4,
             log_level="debug",
         )
     except Exception as e:
         logger.error(f"Failed to start server: {e}")
-        sys.exit(1)
